f56b84d5ccb7f8c4d3a3d5dfa952c59c3c0d07a6
utsav thapa <utsavshumsherthapa@gmail.com>
2022-01-22 08:48:45 +0545

    calc

--- calc.py
+++ calc.py
@@ -1,7 +1,3 @@
-# Python program to create a simple GUI
-# calculator using Tkinter
-
-# import everything from tkinter module
 from tkinter import *
 
 # globally declare the expression variable
@@ -9,43 +5,34 @@ expression = ""
 
 
 # Function to update expression
-# in the text entry box
+
 def press(num):
-	# point out the global expression variable
+	
 	global expression
 
-	# concatenation of string
+	
 	expression = expression + str(num)
 
-	# update the expression by using set method
+	
 	equation.set(expression)
 
 
 # Function to evaluate the final expression
 def equalpress():
-	# Try and except statement is used
-	# for handling the errors like zero
-	# division error etc.
-
-	# Put that code inside the try block
-	# which may generate the error
+	# Try and except statement
 	try:
 
 		global expression
 
-		# eval function evaluate the expression
-		# and str function convert the result
-		# into string
+
 		total = str(eval(expression))
 
 		equation.set(total)
 
-		# initialize the expression variable
-		# by empty string
+		
 		expression = ""
 
-	# if error is generate then handle
-	# by the except block
+	
 	except:
 
 		equation.set(" error ")
@@ -59,8 +46,6 @@ def clear():
 	expression = ""
 	equation.set("")
 
-
-# Driver code
 if __name__ == "__main__":
 	# create a GUI window
 	gui = Tk()
@@ -74,8 +59,6 @@ if __name__ == "__main__":
 	# set the configuration of GUI window
 	gui.geometry("270x150")
 
-	# StringVar() is the variable class
-	# we create an instance of this class
 	equation = StringVar()
 
 	# create the text entry box for
@@ -84,13 +67,12 @@ if __name__ == "__main__":
 
 	# grid method is used for placing
 	# the widgets at respective positions
-	# in table like structure .
+
 	expression_field.grid(columnspan=4, ipadx=70)
 
 	# create a Buttons and place at a particular
 	# location inside the root window .
-	# when user press the button, the command or
-	# function affiliated to that button is executed .
+	
 	button1 = Button(gui, text=' 1 ', fg='black', bg='red',
 					command=lambda: press(1), height=1, width=7)
 	button1.grid(row=2, column=0)
@@ -160,3 +142,9 @@ if __name__ == "__main__":
 	Decimal.grid(row=6, column=0)
 	# start the GUI
 	gui.mainloop()
+
+
+
+
+
+
